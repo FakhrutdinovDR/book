2f914bb9452ea1a27c23aac6d8047d513b35fb16
Denis <skagents@yandex.ru>
2023-06-10 01:05:16 +0700

    Written book getting code(needed correcting)

---
+++ services/handling_book_getting.py
@@ -0,0 +1,29 @@
+import re
+import os
+
+BOOK_PATH = os.path.join(os.getcwd().strip('services'), 'books\Bulgakov.txt')
+page_size = 1050
+book: dict[int, str] = {}
+
+pattern = '[^\w\s]+'
+def _get_part_text(text: str, start, page_size):
+    result = re.finditer(pattern, text[start:])
+    for i in result:
+        if i.end() > page_size:
+            break
+        result = i.end()
+    return text[start:result+start], result
+
+def prepare_book(path: str):
+    with open(path) as file:
+        books = file.read()
+        count_page, start = 1, 0
+        generate_page = _get_part_text(books, start, page_size)
+        book.setdefault(count_page, generate_page[0])
+        while len(books) - start > page_size:
+            count_page, start = count_page + 1, start + generate_page[1]
+            generate_page = _get_part_text(books, start, page_size)
+            book.setdefault(count_page, generate_page[0].lstrip())
+
+
+
