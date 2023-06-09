d50b82ed80ab4bb5204e96188a0aed8a2aac5d1e
Denre <denis.fahrutdinov95@gmail.com>
2023-06-09 15:25:16 +0700

    Correct import dict lexicon dict for handlers

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,15 +1,15 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from lexicon.lexicon import MENU_COMMANDS
+from lexicon.lexicon import ANSWER_MENU_COMMANDS
 
 
 router: Router = Router()
 
 @router.message(F.text == '/start')
 async def proccessingstart(message: Message):
-    await message.answer(text=MENU_COMMANDS['start'])
+    await message.answer(text=ANSWER_MENU_COMMANDS['/start'])
 
 @router.message(F.text == '/help')
 async def proccessinghelp(message: Message):
-    await message.answer(text=MENU_COMMANDS['help'])
+    await message.answer(text=ANSWER_MENU_COMMANDS['/help'])
 
