705bc634751104af98a260fb4ab2aa0a10913bfb
Denre <denis.fahrutdinov95@gmail.com>
2023-06-09 15:22:53 +0700

    Added init in bot 'Menu' button

--- bot.py
+++ bot.py
@@ -2,12 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, loadtoken
 from handlers import user_handlers, other_handlers
+from keyboards.set_menu_button import set_main_menu
 
 async def main() -> None:
     config: Config = loadtoken()
 
     bot: Bot = Bot(config.bot.token)
     dp: Dispatcher = Dispatcher()
+
+    await set_main_menu(bot)
 
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
