9cc83ef520ad8e35ccb6458b8d1a9213f39937ae
Denre <denis.fahrutdinov95@gmail.com>
2023-06-10 13:14:31 +0700

    correct 1 handler

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,12 +1,14 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from lexicon.lexicon import ANSWER_MENU_COMMANDS
-
+from database.db import User, writeupdateusers, USERS
 
 router: Router = Router()
 
 @router.message(F.text == '/start')
 async def proccessingstart(message: Message):
+    USERS.setdefault(str(message.from_user.id), User(message.from_user.id, set()))
+    writeupdateusers(USERS)
     await message.answer(text=ANSWER_MENU_COMMANDS['/start'])
 
 @router.message(F.text == '/help')
