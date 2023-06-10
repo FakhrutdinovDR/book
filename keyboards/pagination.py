b52dc9b4e50fb3916c55f3db3a0c8cf7a582fcb1
Denre <denis.fahrutdinov95@gmail.com>
2023-06-10 13:56:45 +0700

    Correct errors in func

--- keyboards/pagination.py
+++ keyboards/pagination.py
@@ -2,12 +2,12 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon import LEXICON_PAGE_BUTTONS
 
-def create_pagination_keyboard(buttons: str) -> InlineKeyboardMarkup:
+def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 
-    kb_builder.row(*[[InlineKeyboardButton(
+    kb_builder.row(*[InlineKeyboardButton(
         text=LEXICON_PAGE_BUTTONS[button] if button in LEXICON_PAGE_BUTTONS else button,
-        callback_data=button] for button in buttons])
+        callback_data=button) for button in buttons])
 
     return kb_builder.as_markup()
 
