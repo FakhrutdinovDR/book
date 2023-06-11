527486738a26accd8047ef0b740f1a9446830073
Denis <skagents@yandex.ru>
2023-06-11 18:01:12 +0700

    Correct inline button count

--- keyboards/pagination.py
+++ keyboards/pagination.py
@@ -1,12 +1,13 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon import LEXICON_PAGE_BUTTONS
+from services.handling_book_getting import book
 
 def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 
     kb_builder.row(*[InlineKeyboardButton(
-        text=LEXICON_PAGE_BUTTONS[button] if button in LEXICON_PAGE_BUTTONS else button,
+        text=LEXICON_PAGE_BUTTONS[button] if button in LEXICON_PAGE_BUTTONS else f'{button} из {len(book)}',
         callback_data=button) for button in buttons])
 
     return kb_builder.as_markup()
