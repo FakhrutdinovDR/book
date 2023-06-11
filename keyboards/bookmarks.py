1647a4fc294affb740ec404aa03c3bdb1997ac9c
Denis <skagents@yandex.ru>
2023-06-11 19:13:23 +0700

    Correct func to generate bookmarks

--- keyboards/bookmarks.py
+++ keyboards/bookmarks.py
@@ -6,7 +6,7 @@ from services.handling_book_getting import book
 def create_bookmarks_kb(*args: int) -> InlineKeyboardMarkup:
     kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
     for button in sorted(args):
-        kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[button][:100]}', callback_data=f'{str(button)}'))
+        kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[button][:100]}', callback_data=f'{str(button)}-'))
     kb_builder.row(InlineKeyboardButton(text=BOOKMARK_COMMANDS['edit_bookmarks'], callback_data='edit_bookmarks'),
                    InlineKeyboardButton(text=BOOKMARK_COMMANDS['cancel'], callback_data='cancel'), width=2)
     return kb_builder.as_markup()
