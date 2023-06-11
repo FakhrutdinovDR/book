6f873dea730efbb89464a9c7a4e1b12ef23cd02a
Denis <skagents@yandex.ru>
2023-06-11 17:03:21 +0700

    Init func kb bookmarks

---
+++ keyboards/bookmarks.py
@@ -0,0 +1,19 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon.lexicon import BOOKMARK_COMMANDS
+from services.handling_book_getting import book
+
+def create_bookmarks_kb(*args: int) -> InlineKeyboardMarkup:
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    for button in sorted(args):
+        kb_builder.row(InlineKeyboardButton(text=f'{button} - {book[button][:100]}', callback_data=f'{str(button)}'))
+    kb_builder.row(InlineKeyboardButton(text=BOOKMARK_COMMANDS['edit_bookmarks'], callback_data='edit_bookmarks'),
+                   InlineKeyboardButton(text=BOOKMARK_COMMANDS['cancel'], callback_data='cancel'), width=2)
+    return kb_builder.as_markup()
+
+def create_edit_bookmarks_kb(*args: int) -> InlineKeyboardMarkup:
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    for button in sorted(args):
+        kb_builder.row(InlineKeyboardButton(text=f'{BOOKMARK_COMMANDS["del"]} {button} - {book[button][:100]}', callback_data=f'{str(button)}'))
+    kb_builder.row(InlineKeyboardButton(text=BOOKMARK_COMMANDS['cancel'], callback_data='cancel'), width=2)
+    return kb_builder.as_markup()
