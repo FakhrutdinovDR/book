6f05ac0b2d7256dee1ca46a47cb3d79aa9fcb8d6
Denis <skagents@yandex.ru>
2023-06-11 19:12:18 +0700

    Correct filter func for handler bookmarks show

--- filters/filters.py
+++ filters/filters.py
@@ -3,7 +3,7 @@ from aiogram.types import CallbackQuery
 
 class IsDigitCallbackData(BaseFilter):
     async def __call__(self, cb: CallbackQuery) -> bool:
-        return isinstance(cb.data, str) and cb.data.isdigit()
+        return isinstance(cb.data, str) and '-' in cb.data and cb.data[:-1].isdigit()
 
 class IsDelBookmarkCallbackData(BaseFilter):
     async def __call__(self, cb: CallbackQuery) -> bool:
